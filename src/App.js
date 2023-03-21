aa6f28485b7c861c44ede88af23a17a0e60de13b
Jonathan Nanno <jon.nanno@gmail.com>
2023-03-21 19:01:59 -0400

    Added react router, rearranged app return content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import "./App.css";
-import Home from "./components/body/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/pages/Home";
 import Header from "./components/header/Header";
+import About from "./components/pages/About";
+
+import "./App.css";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Home />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
