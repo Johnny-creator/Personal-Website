c9860ee5bb68c905f802dfafb93dc4bba67efa55
Jonathan Nanno <jon.nanno@gmail.com>
2023-03-20 20:33:56 -0400

    Added new files for home page

---
+++ src/components/body/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <p> Hi, my name is Jonathan Nanno and welcome to my website! </p>
+      <a href="/"> Resume </a>
+      <a href="/"> LinkedIn </a>
+      <a href="/"> Github </a>
+    </>
+  );
+};
+
+export default Home;
