13139f31b97e7994fecf54d365d0557381f6da07
Jonathan Nanno <jon.nanno@gmail.com>
2023-04-03 16:19:07 -0400

    Adjusted height of all pages, added content to home, fixed navbar position

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,8 +9,11 @@ const Home = () => {
     <div className={classes.body}>
       <h1> Freelance Front-end developer </h1>
       <img className={classes.img} src={testPhoto} alt="Personal Photo" />
-      <p> Hi, my name is Jonathan Nanno </p>
-      <MediaLinks size="3x"/>
+      <section className={classes.introduction}>
+        <h3> Hi, my name is Jonathan Nanno and welcome to my website</h3>
+        <p> I am a Lambton College CPRO graduate who has a focus for front-end web development. My skills however aren't limited solely to front-end
+          as I've handled working with databases, creating backends to give sites functionality, deploying sites and giving them their own domains, and a lot more. </p>
+      </section>
     </div>
   );
 };
