b7659a1c097b22caad161ca9e29af1998f16be5a
Jonathan Nanno <jon.nanno@gmail.com>
2023-04-06 15:17:22 -0400

    Adjustments to navbar, significant CSS changes for home page, CSS changes for media links

--- src/components/utility/MediaLinks.js
+++ src/components/utility/MediaLinks.js
@@ -3,14 +3,41 @@ import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faFile } from "@fortawesome/free-solid-svg-icons";
 
-import classes from './MediaLinks.module.css';
+import classes from "./MediaLinks.module.css";
 
 const MediaLinks = (props) => {
-    return <>
-        <a className={classes.icon} href="/" target="_blank" rel="noreferrer"> {<FontAwesomeIcon icon={faFile} size={props.size} />} <span>{props.name1}</span> </a>
-        <a className={classes.icon} href="https://www.linkedin.com/in/jonathan-n-3698091b7/" target="_blank" rel="noreferrer"> {<FontAwesomeIcon icon={faLinkedin} size={props.size} />} <span>{props.name2}</span> </a>
-        <a className={classes.icon} href="https://github.com/Johnny-creator" target="_blank" rel="noreferrer"> {<FontAwesomeIcon icon={faGithub} size={props.size} />} <span>{props.name3}</span> </a>
+  return (
+    <>
+      <div className={classes.container}>
+        <a className={classes.icon}
+          href="/" 
+          target="_blank" 
+          rel="noreferrer"
+        >
+          {<FontAwesomeIcon icon={faFile} size={props.size} />}
+          <span>{props.name1}</span>
+        </a>
+        <a
+          className={classes.icon}
+          href="https://www.linkedin.com/in/jonathan-n-3698091b7/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {<FontAwesomeIcon icon={faLinkedin} size={props.size} />}
+          <span>{props.name2}</span>
+        </a>
+        <a
+          className={classes.icon}
+          href="https://github.com/Johnny-creator"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {<FontAwesomeIcon icon={faGithub} size={props.size} />}
+          <span>{props.name3}</span>
+        </a>
+      </div>
     </>
-}
+  );
+};
 
 export default MediaLinks;
