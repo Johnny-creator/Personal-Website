d14edf382eebb2d2992878f0615ef664b06b5862
Jonathan Nanno <jon.nanno@gmail.com>
2023-05-03 15:48:20 -0400

    Implemented framer-motion animation, removed previous hidden div

--- src/components/utility/ProjectContainer.js
+++ src/components/utility/ProjectContainer.js
@@ -1,14 +1,32 @@
 import React from "react";
+import { motion } from "framer-motion";
 import classes from "./ProjectContainer.module.css";
 
 const ProjectContainer = (props) => {
+  const moveDown = {
+    animate: {
+      scale: 1.3,
+      transition: {
+        duration: 0.3,
+        type: "spring",
+      },
+    },
+  };
+
   return (
-    <div onClick={props.onClick} className={classes.projectContainer}>
+    <motion.div
+      whileHover="animate"
+      onClick={props.onClick}
+      className={classes.projectContainer}
+    >
       <h1> {props.name} </h1>
       <img src={props.img} alt={props.imageName} />
       <p> Information about the project </p>
-      <p className={classes.hidden}> Click for more Information </p>
-    </div>
+      <motion.div variants={moveDown} className={classes.click}>
+        {" "}
+        <p> Click for more Information </p>{" "}
+      </motion.div>
+    </motion.div>
   );
 };
 
