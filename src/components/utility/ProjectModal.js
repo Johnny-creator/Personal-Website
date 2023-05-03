c7667c47a41d6bbef82dba3017be2ca3482f0512
Jonathan Nanno <jon.nanno@gmail.com>
2023-05-03 15:47:37 -0400

    Moved img to be displayed above the header

--- src/components/utility/ProjectModal.js
+++ src/components/utility/ProjectModal.js
@@ -26,9 +26,9 @@ const ProjectModal = (props) => {
     <>
       <div className={classes.backdrop} onClick={props.hide} >
         <motion.div onClick={(e) => e.stopPropagation()} variants={appearDisappear} initial="hidden" animate="visible" exit="exit" className={classes.modal}>
-          <h1> {props.name} </h1>
+          <img src={props.image}></img>
           <h1>{props.title}</h1>
-          <div className={classes.content}> {props.children} <img src={props.image}></img> </div>
+          <div className={classes.content}> {props.children} </div>
         </motion.div>
       </div>
     </>
