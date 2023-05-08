ef856e913924c69b4542b4f4a432b96093afb98b
Jonathan Nanno <jon.nanno@gmail.com>
2023-05-08 14:39:00 -0400

    File that exports an object containing an array that dynamically changes modal content

---
+++ src/components/utility/ModalArray.js
@@ -0,0 +1,19 @@
+import sarniaSmashPhoto from "./../../images/SarniaSmashSeries.jpg";
+import simpleSitePhoto from "./../../images/SimpleSiteMaker.jpg";
+
+export default [
+  {
+    title: "Sarnia Smash Brothers",
+    image: sarniaSmashPhoto,
+    content:
+      "A website created solely with HTML, CSS, and JavaScript for a local tournament scene for the videogame 'Super Smash Brothers Ultimate'. The website features a responsive design, accessiblity, a contact form, and more.",
+      alt: "Sarnia Smash Brothers Image"
+  },
+  {
+    title: "Simple Site Maker",
+    image: simpleSitePhoto,
+    content:
+      "A website created using Flask, Python, and Jinja as a school project and completed within two weeks. The website features a responsive design, user authentication (login, logout), the ability to generate basic single-page html sites and download them onto your PC.",
+      alt: "Simple Site Maker Image",
+  },
+];
