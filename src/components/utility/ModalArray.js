0836755bc8e00d74d80812d2506b6d96614b323c
Jonathan Nanno <jon.nanno@gmail.com>
2023-05-31 19:27:34 -0400

    Slight alterations and additions to contents

--- src/components/utility/ModalArray.js
+++ src/components/utility/ModalArray.js
@@ -6,14 +6,14 @@ export default [
     title: "Sarnia Smash Brothers",
     image: sarniaSmashPhoto,
     content:
-      "A website created solely with HTML, CSS, and JavaScript for a local tournament scene for the videogame 'Super Smash Brothers Ultimate'. The website features a responsive design, accessiblity, a contact form, and more.",
+      "A website created solely with HTML, CSS, and JavaScript for a local tournament scene for the videogame 'Super Smash Brothers Ultimate'. The website features a responsive design, accessiblity, a contact form, and more. This website was designed by both Jonathan Nanno and Sheldon Codling.",
       alt: "Sarnia Smash Brothers Image"
   },
   {
     title: "Simple Site Maker",
     image: simpleSitePhoto,
     content:
-      "A website created using Flask, Python, and Jinja as a school project and completed within two weeks. The website features a responsive design, user authentication (login, logout), the ability to generate basic single-page html sites and download them onto your PC.",
+      "A website created using Flask, Python, and Jinja as a school project and completed within two weeks. The website features a responsive design, user authentication (login, logout), the ability to generate basic single-page html sites and download them onto your PC. This website was designed by both Jonathan Nanno and Sheldon Codling, with Jonathan focusing more on the front-end and Sheldon focusing more on the back-end as well as hosting.",
       alt: "Simple Site Maker Image",
   },
 ];
