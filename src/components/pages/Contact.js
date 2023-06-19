677890130b9f1553811f5659a52c700bab36ddff
Jonathan Nanno <jon.nanno@gmail.com>
2023-06-19 15:54:28 -0400

    Implemented onChange and submission confirmation, as well as Netlify React function

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Contact.module.css";
 import{ motion } from 'framer-motion';
 import { useForm } from "react-hook-form";
@@ -6,8 +6,24 @@ import { useForm } from "react-hook-form";
 const Contact = () => {
   const {register, handleSubmit, formState: {errors} } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setIsSubmitted(true);
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
   }
 
   return (
@@ -21,17 +37,18 @@ const Contact = () => {
 
           <label> Name </label>
           {errors.name && <p className={classes.invalid}> Name is required </p>}
-          <input {...register("name", {required: true })} name="name" type="text"></input>
+          <input {...register("name", {required: true, onChange: () => {setIsSubmitted(false);} })} name="name" type="text"></input>
           
           <label> Email </label>
           {errors.email && <p className={classes.invalid}> Email is required </p>}
-          <input {...register("email", { required: true })} name="email" type="email"></input>
+          <input {...register("email", { required: true, onChange: () => {setIsSubmitted(false);} })} name="email" type="email"></input>
           
           <label> Message </label>
           {errors.message && <p className={classes.invalid}> Message is required </p>}
-          <textarea {...register("message", { required: true })} name="message"></textarea>
+          <textarea {...register("message", { required: true, onChange: () => {setIsSubmitted(false);} })} name="message"></textarea>
           
           <button type="submit"> Submit </button>
+          {isSubmitted && <p> Form has been submitted </p>}
         </form>
 
       </motion.div>
